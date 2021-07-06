021a14af2fef49ecc739d6305f099e13a128c5d7
ujjaval <ujjavaldesai5891@gmail.com>
2021-07-06 11:35:09 -0700

    Updated CORS config.

--- server/search-suggestion-service/src/main/java/com/ujjaval/ecommerce/searchsuggestionservice/config/CORSFilter.java
+++ server/search-suggestion-service/src/main/java/com/ujjaval/ecommerce/searchsuggestionservice/config/CORSFilter.java
@@ -1,44 +1,17 @@
 package com.ujjaval.ecommerce.searchsuggestionservice.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@Component
-public class CORSFilter implements Filter {
-
-    private final Logger log = LoggerFactory.getLogger(CORSFilter.class);
-
-    public CORSFilter() {
-        log.info("CORSFilter init");
-    }
-
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer
+{
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-
-        HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
-
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
-
-        chain.doFilter(req, res);
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("https://shoppers-ecom-app.herokuapp.com")
+                .allowedMethods("GET", "POST", "OPTIONS", "DELETE");
     }
-
-    @Override
-    public void init(FilterConfig filterConfig) {
-    }
-
-    @Override
-    public void destroy() {
-    }
-
 }
